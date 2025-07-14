0c374a28141325b15aea5f445758a2925266c97a
whimsy <lofu6@yahoo.com>
2025-07-14 14:28:39 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
+
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document 1987</title>
+  <title>Shopping Cart</title>
 </head>
 <body>
   
